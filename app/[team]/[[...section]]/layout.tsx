9c7097997b87597404b8211a6b7ecdac569aa8c5
goncy <gonzalo.pozzo4@gmail.com>
2024-05-19 13:02:35 -0300

    Refactor layout

--- app/[team]/[[...section]]/layout.tsx
+++ app/[team]/[[...section]]/layout.tsx
@@ -7,7 +7,11 @@ export default function SectionLayout({
 }) {
   return (
     <>
-      {nav}
+      <nav className="bg-white shadow">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          {nav}
+        </div>
+      </nav>
       {children}
     </>
   );
